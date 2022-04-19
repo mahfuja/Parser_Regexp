96a775e4460643daf6ef9e0e1daaa1551cefc5a7
Mst. Mahfuja Akter <50299921+mahfuja@users.noreply.github.com>
2022-04-19 09:12:28 +0200

    Update wordnet_data_parser.py

--- wordnet_data_parser.py
+++ wordnet_data_parser.py
@@ -1,57 +1,32 @@
-#!/usr/bin/python
-
 import sys
 import re
-
 file_ = open(sys.argv[1], 'r').read()
-FO = open('output_file.txt', 'w')
-FA = open('output_text.txt', 'w')
 
 #logRegex = "^ <synset id=\"(.*)\".ofs=.*\n..<terms>[\s\S]*?<\/terms>\n..<keys>[\s\S]*?<\/keys>\n..<gloss desc=\"orig\">\n...<orig>(.*)<\/orig>"
 logRegex = "^ <synset id=\"(.*)\".ofs=.*\n..<terms>\n...<term>(.*)<\/term>[\s\S]*?<\/terms>\n..<keys>[\s\S]*?<\/keys>\n..<gloss desc=\"orig\">\n...<orig>(.*)<\/orig>"
-data = []
-data1 = []
+entity2text_all = {}
+entity2name_all = {}
 
-def dataParsing(file_,data):
-    match = re.findall(logRegex,file_, re.M)
-    print(len(match))
-    for i in range(0,len(match)):
-        st = re.sub("[\"]", "\\\"", match[i][2])
-        FO.write("%s\t\"%s\"@en\n" %(match[i][0],st))
-        FA.write("%s\t%s\n" %(match[i][0],match[i][1]))
-				
-dataParsing(file_,data)
+match = re.findall(logRegex,file_, re.M)
+for i in range(0,len(match)):
+    desc = re.sub("[\"]", "\\\"", match[i][2])
+    entity2text_all[match[i][0]] = "\""+desc+"\"@en"
+    entity2name_all[match[i][0]] = match[i][1]
 
-FO.close()
-file1 = open('entity2id.txt','r')
-file2 = open('output_file.txt', 'r')
-FINAL = open('WN9_eid2description.txt', 'w')
+entity2text = {}
+entity2name = {}
 
-file3 = open('output_text.txt', 'r')
-FINAL2 = open('eid2text.txt', 'w')
-for line in file1:
-    l = line.split("\t")
-    data1.append(l[0])
-datafile = file2.readlines()
-count = 0
-for line in datafile:
-    entity = line.split("\t")
-    if entity[0] in data1:
-        FINAL.write("%s" %(line))
-        count = count+1
-FINAL.close()
-file1.close()
-file2.close()
+to_read_ent = open('ent2id', 'r')
+for line in to_read_ent.readlines():
+    ent, txt = line.strip().split('\t')
+    if ent not in entity2text_all.keys():
+        continue
+    entity2text[ent] = entity2text_all[ent]
+    entity2name[ent] = entity2name_all[ent]
+to_read_ent.close()
 
-textfile = file3.readlines()
-count = 0
-for line in textfile:
-        entity = line.split("\t")
-        if entity[0] in data1:
-                FINAL2.write("%s" %(line))
-                count = count+1
-file3.close()
-FINAL2.close()
-
-
-
+for (dic, f) in zip([entity2text, entity2name], ['description.txt', 'entities.dict']):
+    ff = open(f, 'w+')
+    for (x, i) in dic.items():
+        ff.write("{}\t{}\n".format(x, i))
+    ff.close()
